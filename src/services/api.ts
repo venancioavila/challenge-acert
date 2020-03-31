ab33143e359a52415451a3b435fd0feac225d454
Venancio Souza <venanciodesouza@icloud.com>
2020-03-31 17:14:46 -0300

    fix problems

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,13 @@
 export const api = async (method: string, search: string) => {
   const res = await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=${method}.search&artist=${search}&api_key=2f56f55df943c0e9b920041c41530de2&format=json`
+    `https://ws.audioscrobbler.com/2.0/?method=${method}.search&artist=${search}&api_key=2f56f55df943c0e9b920041c41530de2&format=json`
   );
   return await res.json();
 };
 
 export const getAlbums = async (album: string) => {
   const res = await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${album}&api_key=2f56f55df943c0e9b920041c41530de2&format=json`
+    `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${album}&api_key=2f56f55df943c0e9b920041c41530de2&format=json`
   );
   return await res.json();
 };
